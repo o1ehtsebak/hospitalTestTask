3043dacdf886aa4522af3d942701b182e9e3a7a0
Oleh Tsebak <oleg.tsebak16@gmail.com>
2024-09-24 00:52:06 +0300

    Fix test

--- src/test/java/com/department/hospital/HospitalApplicationTests.java
+++ src/test/java/com/department/hospital/HospitalApplicationTests.java
@@ -1,9 +1,11 @@
 package com.department.hospital;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+
+@ExtendWith(MockitoExtension.class)
 class HospitalApplicationTests {
 
 	@Test
