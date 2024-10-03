6c95239f2fd2a260e5300c4f708fd660a5cbca2c
Oleh Tsebak <oleg.tsebak16@gmail.com>
2024-10-04 00:22:41 +0300

    Get rid of cascade type

--- src/main/java/com/department/hospital/component/department/Department.java
+++ src/main/java/com/department/hospital/component/department/Department.java
@@ -18,14 +18,11 @@ public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-
 	@Column(unique = true)
 	private String name;
-
-	@OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "department")
 	private List<Room> rooms;
-
-	@ManyToMany(cascade = CascadeType.ALL)
+	@ManyToMany
 	@JoinTable(name = "department2doctor", joinColumns = @JoinColumn(name = "department_id"), inverseJoinColumns = @JoinColumn(name = "doctor_id"), uniqueConstraints = { @UniqueConstraint(columnNames = { "department_id", "doctor_id" }) })
 	private List<Doctor> doctors;
 
